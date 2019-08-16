1a81e95622e45ca1cd69360799eb4e2f3e31bcf3
Nicole Jiang <apple80177@gmail.com>
2019-08-16 18:01:54 +0800

    fixed dataframe drop

--- SciPy/pyPackage.py
+++ SciPy/pyPackage.py
@@ -1,12 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 
-tables = pd.read_csv('train.csv')
-totalPrice = tables["total_price"]
-buildingArea = tables["building_area"]
-landArea = tables["land_area"]
-
+tables = pd.read_csv('./train.csv')
 table = tables.loc[:,['building_area', 'land_area', 'total_price']]
 
 plt.scatter(table['building_area'], table['total_price'])
@@ -20,16 +15,31 @@ plt.xlabel("land_area")
 plt.ylabel("total_price")
 plt.show()
 
-
-buildingArea_table = table[(table["building_area"] <= 1000) & (table["total_price"] <= 300000000)]
-landArea_table = table[(table["land_area"] <= 2000) & (table["total_price"] <= 300000000)]
-plt.scatter(buildingArea_table["building_area"] , buildingArea_table["total_price"])
+testTable = table[(table["land_area"] <= 2000) & (table["total_price"] <= 300000000) & (table["building_area"] <= 1000)]
+plt.scatter(testTable["building_area"] , testTable["total_price"])
 plt.title("Building Area - After")
 plt.xlabel("building_area")
 plt.ylabel("total_price")
 plt.show()
-plt.scatter(landArea_table["land_area"], landArea_table["total_price"])
+plt.scatter(testTable["land_area"], testTable["total_price"])
 plt.title("Land Area - After")
 plt.xlabel("land_area")
 plt.ylabel("total_price")
 plt.show()
+
+table = table.drop(table[table["building_area"]>1000].index)
+table = table.drop(table[table["total_price"]>300000000].index)
+table = table.drop(table[table["land_area"]>2000].index)
+plt.scatter(table["building_area"] , table["total_price"])
+plt.title("Building Area - After")
+plt.xlabel("building_area")
+plt.ylabel("total_price")
+plt.show()
+plt.scatter(table["land_area"], table["total_price"])
+plt.title("Land Area - After")
+plt.xlabel("land_area")
+plt.ylabel("total_price")
+plt.show()
+
+#print(table["land_area"]<10)
+#print(table[table["land_area"]<10].index)
